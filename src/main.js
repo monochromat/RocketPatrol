0ee8c48e3b1471836175e709490a6bd6f4a24db8
monochromat <efrainluengas2@gmail.com>
2021-04-28 02:47:31 -0700

    3rd

--- src/main.js
+++ src/main.js
@@ -5,4 +5,7 @@ let config = {
     scene: [ Menu, Play ]
   }
 let game = new Phaser.Game(config);
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
 //console.log("poop")ddd
