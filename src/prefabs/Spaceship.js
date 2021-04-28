f416a380296790770789a871b0f56c2db1c7d3ec
monochromat <efrainluengas2@gmail.com>
2021-04-28 03:32:51 -0700

    6th

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,18 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.moveSpeed = 3;
+    }
+
+    update() {
+        // mv left
+        this.x -= this.moveSpeed;
+        // wrap
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        }
+    }
+}
